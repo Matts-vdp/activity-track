fcaeb9d177f864b1c11d82161f3058f4cae2793d
Matts <vdpmatts@gmail.com>
2022-05-19 10:38:41 +0200

    add basic dashboard

---
+++ src/app/dash-item/dash-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dash-item',
+  templateUrl: './dash-item.component.html',
+  styleUrls: ['./dash-item.component.css']
+})
+export class DashItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @Input() name!: string;
+  @Input() id!: number;
+
+}
