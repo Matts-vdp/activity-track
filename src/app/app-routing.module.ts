fb18e4610094c3a6bebdb13a60e0e1ccfdbc9e6e
Matts <vdpmatts@gmail.com>
2022-05-19 16:43:40 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch:'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'activities', component: ActivityListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
